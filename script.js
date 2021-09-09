69c5aec04dcf8080bb7039cbc8102a95866e241e
Aj <ahmedjalalsd@gmail.com>
2021-09-09 21:00:48 +0100

    add select and option elements and  populate them

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 //You can edit ALL of the code here
+
 const rootElem = document.getElementById("root");
 const allEpisodes = getAllEpisodes();
+
 function setup() {
   // const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
 
+  makePageForEpisodes(allEpisodes);
+  displaySearchResultCount(allEpisodes, allEpisodes);
+  CreateSelectOptions();
+
+  // keyup event on of JS event listeners
   let searchBox = document.getElementById("site-search");
   searchBox.addEventListener("keyup", filterEpisodes);
-
-  // keyup event for eventlistern
 }
 
 function filterEpisodes(event) {
   // console.log(event.target.value);
   // let searchBox = document.getElementById("site-search");
   // searchBox.value === event.target.value
-  const allEpisodes = getAllEpisodes();
 
   let filteredEpisodes = allEpisodes.filter((ele) => {
     if (ele.name.toLowerCase().includes(event.target.value.toLowerCase())) {
-      // console.log(true);
       return true;
     } else {
       return false;
     }
   });
   // console.log(filteredEpisodes);
-  // search bar
-
-  const searchContainerElm = document.getElementById("search-container");
-  const searchPara = document.createElement("p");
-  searchPara.textContent = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
-  searchContainerElm.appendChild(searchPara);
+  displaySearchResultCount(filteredEpisodes, allEpisodes);
 
   makePageForEpisodes(filteredEpisodes);
 }
@@ -44,12 +41,36 @@ function makePageForEpisodes(episodeList) {
   episodeList.forEach(createCard);
 }
 
+function displaySearchResultCount(filteredEpisodes, allEpisodes) {
+  // create a search result counter
+  const searchContainerElm = document.getElementById("search-container");
+  const searchResultCount = document.getElementById("result-count");
+
+  // console.log(searchContainerElm.childNodes);
+  // console.log(searchContainerElm.removeChild[2]);
+
+  searchContainerElm.removeChild(searchContainerElm.lastElementChild);
+  searchResultCount.textContent = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
+  searchContainerElm.appendChild(searchResultCount);
+
+  // console.log(searchContainerElm.lastChild);
+}
+
+function CreateSelectOptions() {
+  allEpisodes.forEach(populateSelectOptions);
+}
+
+function populateSelectOptions(episode, index) {
+  const selectElm = document.getElementById("episode-select");
+  // selectElm.options.length gets the current index
+  selectElm.options[selectElm.options.length] = new Option(
+    `${formatEpisodeNaming(episode)} - ${episode.name}`,
+    episode.name
+  );
+}
+
 function createCard(episode, index, array) {
   // console.log(episode.name);
-  // console.log(episode.season);
-  // console.log(episode.number);
-  // console.log(episode.image.medium);
-  // console.log(episode.summary);
 
   // Main div which hold the content of the episode
   let card = document.createElement("div");
@@ -71,8 +92,10 @@ function createCard(episode, index, array) {
   let spanElmEpisodeCode = document.createElement("span");
   let spanElmSummary = document.createElement("span");
 
-  titleCard.textContent = episode.name;
-  spanElmEpisodeCode.textContent = ` - S0${episode.season}E0${episode.number}`;
+  // console.log(formatEpisodeNaming(episode));
+
+  titleCard.textContent = episode.name + " - ";
+  spanElmEpisodeCode.textContent = formatEpisodeNaming(episode);
   spanElmSummary.innerHTML = episode.summary;
 
   titleCard.appendChild(spanElmEpisodeCode);
@@ -85,6 +108,18 @@ function createCard(episode, index, array) {
   imgDiv.className = "img-container";
 
   rootElem.appendChild(card).appendChild(textContentDiv);
+}
+
+function formatEpisodeNaming(episode) {
+  if (episode.number > 9) {
+    // return ` - S0${episode.season}E${episode.number}`;
+    return (
+      episode.season.toString().padStart(3, "S0") +
+      episode.number.toString().padStart(3, "E0")
+    );
+  } else {
+    return `S0${episode.season}E0${episode.number}`;
+  }
 }
 
 window.onload = setup;
